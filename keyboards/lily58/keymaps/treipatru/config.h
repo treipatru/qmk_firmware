16523dcdb9eeefd438eec46636ab19e7583c5a42
treipatru <andrei@planet34.org>
2023-04-25 16:06:55 +0200

    update lily58 profile

--- keyboards/lily58/keymaps/treipatru/config.h
+++ keyboards/lily58/keymaps/treipatru/config.h
@@ -36,8 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
-#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 
 // Underglow
 /*
